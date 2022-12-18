dcfef18dc4bda61108d33c02c59bc9f495431267
SamanthaSalandy <115196193+SamanthaSalandy@users.noreply.github.com>
2022-12-18 13:47:58 -0400

    Add files via upload

---
+++ Common/footer.php
@@ -0,0 +1,42 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-one md-mb-30 sm-mb-30">
+            <h3>Centre for Information and Communcation Technology</h3>
+            <p>In the beginning was the truth and the truth laid in all of us and we began to know what was good. We learnt the language of computers and now they will unlock the magic of us. Sleep tight and good night.
+            </p>
+          </div>
+          <div class="col-md-2 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>Quick Links</h2>
+            <ul>
+              <li><a href="">About CICT</a></li>
+              <li><a href="">Admissions Policy</a></li>
+              <li><a href="">Apply for Admission</a></li>
+              <li><a href="">Download Prospectus</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>UNDERGRADUATE PROGRAMS</h2>
+            <ul>
+              <li><a href="">B.Sc. Civil Engineering</a></li>
+              <li><a href="">B.Sc. Electrical Engineering</a></li>
+              <li><a href="">B.Sc. Mechanical Engineering</a></li>
+              <li><a href="">B.SC  Cyber Security</a></li>
+              <li><a href="">BS Computer Science</a></li>
+              <li><a href="">Bachelors of Coding and Animation</a></li>
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+          <h2>CONTACT DETAILS</h2>
+              <p>Address: Centre for Information and Communcation Technology,
+                  Wrightson Road, Port of Spain – Trinidad and Tobago</p>
+              <p>Telephones: (+868) 555-5555</p>
+              <p>E-mail: cict.edu.gov.tt</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  <p class="footer-bottom-text">CopyRights © reserved by 95169</p>
+</footer>
